641582b201bbe406bf4dca4efa7fde8c2b530e5f
beijia <beijia@umich.edu>
2024-07-02 11:10:35 -0500

    Initial Commit

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Beijia Chen on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
