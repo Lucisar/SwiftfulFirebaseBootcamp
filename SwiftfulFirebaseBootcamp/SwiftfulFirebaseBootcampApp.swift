56bf76e9687357a62514758540f70548e3fb3a52
beijia <beijia@umich.edu>
2024-07-02 14:26:08 -0500

    Sign In interface and Create User function

--- SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -1,17 +1,26 @@
-//
-//  SwiftfulFirebaseBootcampApp.swift
-//  SwiftfulFirebaseBootcamp
-//
-//  Created by Beijia Chen on 7/2/24.
-//
-
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftfulFirebaseBootcampApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                AuthenticationView()
+            }
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+
+        return true
     }
 }
